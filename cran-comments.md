f691a9be7ea3ec0b64987237c4f8ff6c761d055e
Kevin Wright <kw.stat@gmail.com>
2025-02-04 12:26:34 -0600

    v1.0

--- cran-comments.md
+++ cran-comments.md
@@ -1,10 +1,14 @@
-# connected 0.2
+# connected 1.0
 
-This is a new package.
-
-# ## test environments and results
+## test environments and results
 
 1. R 4.4.2 on Windows 11
-2. WinBuilder R-Devel
-3.
+2. WinBuilder R-devel
+3. WinBuilder R-release
+4. rhub::rhub_check()
 
+Check results:
+
+Possibly misspelled words in DESCRIPTION:
+  Connectedness (2:30)
+  connectedness (7:28, 8:54)
--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("con_check works", {
+  expect_equal(
+    con_check(data_eccleston, ~ row + trt),
+    structure(c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+                1L, 1L, 1L), levels = "1", class = "factor")
+  )
+
+  expect_equal(
+    con_check(data_eccleston, ~ row + col + trt),
+    structure(c(16L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L),
+              levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"),
+              class = "factor")
+  )
 })
+
+test_that("con_view", {
+  set.seed(42)
+  data_fernando = transform(data_fernando,
+                            y=stats::rnorm(9, mean=100))
+  expect_warning(con_view(data_fernando, y ~ gen*herd, cluster=FALSE,
+           main = "Fernando unsorted"))
+})
+
+test_that("con_filter", {
+  tab <- data.frame(gen=c("G1","G1","G1","G1", "G2","G2","G2", "G3"),
+                    state=c("S1","S2","S3","S4", "S1","S2","S4", "S1"))
+  expect_warning( con_filter(tab, ~ 2 * state / gen) )
+  })
